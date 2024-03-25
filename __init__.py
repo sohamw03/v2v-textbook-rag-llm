247481100abbd02e6cc32c6ffd42ad3eef80acc6
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-25 23:12:53 +0530

    test: httpresponse

--- __init__.py
+++ __init__.py
@@ -4,4 +4,5 @@ from yozu_pdf_pipeline.wsgi import application
 
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return func.WsgiMiddleware(application).handle(req, context)
+    # return func.WsgiMiddleware(application).handle(req, context)
+    return func.HttpResponse("Hello from Azure Functions!")
