5090db95df8771af34a3274e82b78093cbf3b089
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-26 02:56:58 +0530

    chore: change names

--- yozu_pdf_pipeline/wsgi.py
+++ yozu_pdf_pipeline/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yozu_pdf_pipeline.settings")
 
 application = get_wsgi_application()
-
-app = application
