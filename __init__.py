a17d36b172e21ca617d9836f2bd2e5d14197ccb6
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-25 23:36:32 +0530

    feat: vercel config

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-
-from yozu_pdf_pipeline.wsgi import application
-
-
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    # return func.WsgiMiddleware(application).handle(req, context)
-    return func.HttpResponse("Hello from Azure Functions!")
