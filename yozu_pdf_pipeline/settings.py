00410304a1f56cd5406e20efe314c06c1bae4ab2
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2024-03-31 19:05:46 +0530

    fix: add static root

--- yozu_pdf_pipeline/settings.py
+++ yozu_pdf_pipeline/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    # "django.contrib.staticfiles",
+    "django.contrib.staticfiles",
     "main",
 ]
 
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
